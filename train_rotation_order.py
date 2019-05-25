d1804b25a3272b04acfbc7d0190b950e7272cc85
Derek Lane <dglane001@gmail.com>
2019-05-25 09:47:04 -0500

    removing SGD change xform_order to rotation_order list lookup in generate samples

--- train_rotation_order.py
+++ train_rotation_order.py
@@ -11,7 +11,7 @@ from keras.optimizers import SGD, Adam
 from keras.preprocessing.image import ImageDataGenerator
 
 # this is the complete list of all rotation orders
-xform_orders = [''.join(list(tuple)) for tuple in list(permutations({'x','y','z'}))]
+all_rotation_orders = [''.join(list(tuple)) for tuple in list(permutations({'x','y','z'}))]
 
 def standardize(x):
     x -= np.mean(x)
@@ -23,29 +23,28 @@ def random_vector(n, mag):
 def generate_sample():
 
     # choose a random order
-    xform_order = random.choice(xform_orders)
+    rotation_order = random.choice(all_rotation_orders)
 
     # generate three angles
-    angles = random_vector(3, 10.0)
+    angles = random_vector(3, 30.0)
 
     # reject if any absolute angle < 0.1 degree
-    if (min(abs(angles)) < 0.01):
+    if (min(abs(angles)) < 0.1):
         return generate_sample()
 
     # compose to create rotate matrix
-    matrix = R.from_euler(xform_order, angles, degrees=True)
+    matrix = R.from_euler(rotation_order, angles, degrees=True)
 
     # actual vector is concatenation of flattened matrix + angles
     error_angles = angles + random_vector(3, 0.1)
-    # experimenting with sin/cos as inputs
-    sincos_angles = [scang for angle in error_angles for scang in [math.sin(angle), math.cos(angle)]] 
     x = np.concatenate((matrix.as_dcm().flatten(), np.array(error_angles)))
     standardize(x)
-    return (x, xform_orders.index(xform_order))
+    return (x, rotation_order)
 
 def generate_samples(n):
-    x, y = list(zip(*[generate_sample() for n in range(n)]))
-    return np.array(x), keras.utils.to_categorical(y)
+    xs, rotation_orders = list(zip(*[generate_sample() for n in range(n)]))
+    ys = [all_rotation_orders.index(rotation_order) for rotation_order in rotation_orders]
+    return np.array(xs), keras.utils.to_categorical(ys)
 
 x_train, y_train = generate_samples(10000)
 x_test, y_test = generate_samples(1000)
@@ -57,7 +56,6 @@ model.add(Dense(16, activation='relu'))
 model.add(Dense(6, activation='softmax'))
 model.summary()
 
-sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9)
 adam = Adam(lr=0.001, decay=0.0)
 model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['accuracy']) 
 

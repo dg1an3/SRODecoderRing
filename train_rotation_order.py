2de5cd06393fcdc83d09ef6abb4d6217c1054c9a
Derek Lane <dglane001@gmail.com>
2019-05-25 16:57:02 -0500

    tinkering

--- train_rotation_order.py
+++ train_rotation_order.py
@@ -20,17 +20,18 @@ def standardize(x):
 def random_vector(n, mag):
     return 2.0 * mag * (0.5-np.random.random(size=n))
 
-def generate_sample():
-
+def generate_sample_from_model():
+    """
+    """
     # choose a random order
     rotation_order = random.choice(all_rotation_orders)
 
     # generate three angles
-    angles = random_vector(3, 30.0)
+    angles = random_vector(3, 10.0)
 
     # reject if any absolute angle < 0.1 degree
     if (min(abs(angles)) < 0.1):
-        return generate_sample()
+        return generate_sample_from_model()
 
     # compose to create rotate matrix
     matrix = R.from_euler(rotation_order, angles, degrees=True)
@@ -42,7 +43,7 @@ def generate_sample():
     return (x, rotation_order)
 
 def generate_samples(n):
-    xs, rotation_orders = list(zip(*[generate_sample() for n in range(n)]))
+    xs, rotation_orders = list(zip(*[generate_sample_from_model() for n in range(n)]))
     ys = [all_rotation_orders.index(rotation_order) for rotation_order in rotation_orders]
     return np.array(xs), keras.utils.to_categorical(ys)
 
@@ -56,9 +57,9 @@ model.add(Dense(16, activation='relu'))
 model.add(Dense(6, activation='softmax'))
 model.summary()
 
-adam = Adam(lr=0.001, decay=0.0)
+adam = Adam(lr=0.001, decay=1e-8)
 model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['accuracy']) 
 
-model.fit(x_train, y_train, epochs=500, batch_size=512)
-score = model.evaluate(x_test, y_test, batch_size=512)
+model.fit(x_train, y_train, epochs=500, batch_size=256)
+score = model.evaluate(x_test, y_test, batch_size=256)
 print(score)

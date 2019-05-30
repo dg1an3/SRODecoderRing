5c155abfbddb18af9af10242881000793d7be740
Derek Lane <dglane001@gmail.com>
2019-05-30 11:52:01 -0500

    adding tests for model and weights

--- SRODecoderEngineTest/SRODecoderEngineUnitTest.cs
+++ SRODecoderEngineTest/SRODecoderEngineUnitTest.cs
@@ -1,7 +1,9 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
 using SRODecoderEngine;
+using System;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Text;
 
@@ -18,13 +20,39 @@ namespace SRODecoderEngineTest
             {
                 var json = reader.ReadToEnd();
                 var sequentialModel = JsonConvert.DeserializeObject<SequentialModel>(json);
-                Assert.IsTrue(sequentialModel.Layers.Count == 1);
+                Assert.IsTrue(sequentialModel.Layers.Count == 2);
+
+                // test that first has batch input shape
+                Assert.IsTrue(sequentialModel.Layers
+                    .First().Configuration.BatchInputShape != null);
+                Assert.IsTrue(sequentialModel.Layers
+                    .Skip(1).All(layer => layer.Configuration.BatchInputShape == null));
+
+                // test that they have different names
+                Assert.IsTrue(sequentialModel.Layers
+                    .Select(layer => layer.Configuration.Name)
+                    .Distinct().Count() 
+                        == sequentialModel.Layers.Count());
+
+                // test if they are trainable
+                Assert.IsTrue(sequentialModel.Layers.All(layer => layer.Configuration.Trainable));
+
+                // test for activation values
+                Assert.IsTrue(sequentialModel.Layers.All(layer => layer.Configuration.Activation != null));
             }
         }
 
         [TestMethod]
         public void TestLoadWeights()
         {
+            SequentialModel sequentialModel = null;
+            var modelStream = GetType().Assembly.GetManifestResourceStream("SRODecoderEngineTest.test_model_config.json");
+            using (var modelReader = new StreamReader(modelStream, Encoding.UTF8))
+            {
+                var json = modelReader.ReadToEnd();
+                sequentialModel = JsonConvert.DeserializeObject<SequentialModel>(json);
+            }
+
             var stream = GetType().Assembly.GetManifestResourceStream("SRODecoderEngineTest.test_model_weights.csv");
             using (var reader = new StreamReader(stream, Encoding.UTF8))
             {
@@ -32,6 +60,33 @@ namespace SRODecoderEngineTest
                 {
                     var line = reader.ReadLine();
                     var values = line.Split(',');
+                    var layer_name = values[0];
+                    var currentLayer = sequentialModel.Layers.First(layer => layer.Configuration.Name == values[0]);
+                    var variable_desc = values[1].Split(new char[] { '/', ':' });
+                    Assert.IsTrue(variable_desc[0].CompareTo(layer_name) == 0);
+
+                    var tensor_height = Convert.ToInt32(values[2]);
+                    var tensor_width = Convert.ToInt32(values[3]);
+                    var tensor_row = Convert.ToInt32(values[4]);
+                    Assert.IsTrue(tensor_row < tensor_height);
+
+                    if (variable_desc[1].CompareTo("kernel") == 0)
+                    {
+                        if (currentLayer.Configuration.BatchInputShape != null)
+                        {
+                            Assert.IsTrue(tensor_height == currentLayer.Configuration.BatchInputShape[1].Value);
+                        }
+                    }
+                    else if (variable_desc[1].CompareTo("bias") == 0)
+                    {
+                        Assert.IsTrue(tensor_height == 1);
+                    }
+                    else
+                    {
+                        Assert.Fail("unrecognized layer variable");
+                    }
+                    Assert.IsTrue(tensor_width == currentLayer.Configuration.Units);
+                    Assert.IsTrue(values.Length - 5 == tensor_width);
                 }
             }
         }

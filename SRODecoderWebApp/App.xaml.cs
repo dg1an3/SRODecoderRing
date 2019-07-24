124bd729430a2bfe1a4eb6ca819fabff1bf17757
Lane, Derek <derek.lane@elekta.com>
2019-07-24 13:59:24 -0500

    adding CSHTML5 test UI project

---
+++ SRODecoderWebApp/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace SRODecoderWebApp
+{
+    public sealed partial class App : Application
+    {
+        public App()
+        {
+            this.InitializeComponent();
+
+            var mainPage = new MainPage();
+            Window.Current.Content = mainPage;
+        }
+    }
+}
---
+++ SRODecoderWebApp/Page6_About.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace SRODecoderWebApp
+{
+    public partial class Page6_About : Page
+    {
+        public Page6_About()
+        {
+            this.InitializeComponent();
+        }
+    }
+}

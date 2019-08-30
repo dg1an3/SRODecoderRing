76d13cc2d1b8eecd01d90eae59325f7391c33a4d
Derek Lane <dglane001@gmail.com>
2019-08-29 20:21:38 -0500

    making the model a bit more realistic; using expressions

--- DecoderUI/Elmish.DecoderUI/Program.fs
+++ DecoderUI/Elmish.DecoderUI/Program.fs
@@ -1,41 +1,78 @@
-﻿open Elmish
+﻿open Microsoft.FSharp.Quotations
 open Elmish
 
+type PatientPosition =
+| HeadFirstSupine
+| HeadFirstDecubitusLeft
+| HeadFirstProne
+| HeadFirstDecubitusRight
+| FootFirstSupine
+| FootFirstDecubitusLeft
+| FootFirstProne
+| FootFirstDecubitusRight
+
+type TransformationOrder =
+| TRxRyRz
+| TRzRyRx
+| RxRyRzT
+| RzRyRxT
+
+type IecCouchShiftModel = 
+    { Tx:decimal; Ty:decimal; Tz:decimal; 
+        Rx:decimal; Ry:decimal; Rz:decimal; }
+
+let initCouchShift = 
+    { Tx=0.0m; Ty = 0.0m; Tz = 0.0m;
+        Rx=0.0m; Ry = 0.0m; Rz = 0.0m; }
+
 type Model = 
-    { SroMatrix : double[];
-        Offset : double[]; 
-        PatientPosition : string;
-        RotationOrder : string }
+    { SroMatrix : decimal[];
+        IecCouchShift : IecCouchShiftModel;
+        GeometryEstimate : PatientPosition * TransformationOrder -> decimal;
+        LockExpression : IecCouchShiftModel -> Expr<bool>;
+        MaximizeExpression : PatientPosition * TransformationOrder -> Expr<bool>; }
 
 let init () = 
     { SroMatrix = [||];
-        Offset = [||]; 
-        PatientPosition = "";
-        RotationOrder = ""; }, Cmd.none
+        IecCouchShift = initCouchShift;
+        LockExpression = (fun offset -> <@ offset.Tx = 0.0m && offset.Ty = 0.0m @>);
+        GeometryEstimate = (fun (patpos,xord) -> 0.0m);
+        MaximizeExpression = (fun (patpos,xord) -> <@patpos = HeadFirstSupine@>); }, 
+        Cmd.none
 
 type Message = 
-| SetSroMatrix of double[]
-| SetPatientPosition of string
-| SetRotationOrder of string
+| LoadSroMatrix of decimal[]
+| UpdateOffset of (IecCouchShiftModel -> IecCouchShiftModel)
+| Lock of (IecCouchShiftModel -> Expr<bool>)
+| Unlock of (IecCouchShiftModel -> Expr<bool>)
+| Maximize of (PatientPosition * TransformationOrder -> Expr<bool>)
 
 let update msg model = 
     match msg with
-    | SetSroMatrix matrix -> 
+    | LoadSroMatrix matrix -> 
         { model with SroMatrix = matrix }, 
             Cmd.none
-    | SetRotationOrder order -> 
-        // update offset
-        let offset = model.Offset
-        { model with RotationOrder = order; Offset = offset }, 
+    | UpdateOffset deltaFunc -> 
+        let newShift = (deltaFunc model.IecCouchShift)
+        { model with IecCouchShift = newShift },
             Cmd.none
-    | SetPatientPosition position -> 
+    | Lock expr -> 
+        // update offset
+        let shift = model.IecCouchShift
+        { model with IecCouchShift = shift; LockExpression = expr },
+            Cmd.none
+    | Unlock expr -> 
         // update offset if matrix is set
-        let offset = model.Offset
-        { model with PatientPosition = position; Offset = offset },
+        let shift = model.IecCouchShift
+        { model with IecCouchShift = shift; LockExpression = expr },
+            Cmd.none
+    | Maximize expr -> 
+        { model with MaximizeExpression = expr },
             Cmd.none
 
 [<EntryPoint>]
 let main argv = 
+
     (fun model _ -> 
         printf "%A\n" model)
     |> Elmish.Program.mkProgram init update
